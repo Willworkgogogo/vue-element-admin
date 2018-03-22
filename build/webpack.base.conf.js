a695f4fc86f5e96efa2a67e32d0a01e13721a8f7
wangweiwei <will_workhard@163.com>
2018-03-23 07:31:29 +0800

    update: 整理webpack.base文件

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,87 +1,90 @@
 'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
+const path            = require('path')
+const utils           = require('./utils')
+const config          = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
 const createLintingRule = () => ({
-  test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
+  test   : /\.(js|vue)$/,
+  loader : 'eslint-loader',
   enforce: 'pre',
   include: [resolve('src'), resolve('test')],
   options: {
-    formatter: require('eslint-friendly-formatter'),
+    formatter  : require('eslint-friendly-formatter'),
     emitWarning: !config.dev.showEslintErrorsInOverlay
   }
 })
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
-  entry: {
+  entry  : {
     app: './src/main.js'
   },
   output: {
-    path: config.build.assetsRoot,
+    path    : config.build.assetsRoot,
     filename: '[name].js',
+    // process.env属性返回一个包含用户环境信息的对象
+    // 这个对象中可以添加自定义的属性，值都是string类型
+    // NODE_ENV也是自己添加到自己环境变量中的值
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      :  config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
-    alias: {
+    alias     : {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@'   : resolve('src'),
     }
   },
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
-        test: /\.vue$/,
-        loader: 'vue-loader',
+        test   : /\.vue$/,
+        loader : 'vue-loader',
         options: vueLoaderConfig
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader?cacheDirectory',
+        test   : /\.js$/,
+        loader : 'babel-loader?cacheDirectory',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
-        test: /\.svg$/,
-        loader: 'svg-sprite-loader',
+        test   : /\.svg$/,
+        loader : 'svg-sprite-loader',
         include: [resolve('src/icons')],
         options: {
           symbolId: 'icon-[name]'
         }
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        test   : /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader : 'url-loader',
         exclude: [resolve('src/icons')],
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name : utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
       {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        test   : /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader : 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+          name : utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        test   : /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader : 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name : utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
     ]
@@ -92,10 +95,10 @@ module.exports = {
     setImmediate: false,
     // prevent webpack from injecting mocks to Node native modules
     // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
+    dgram        : 'empty',
+    fs           : 'empty',
+    net          : 'empty',
+    tls          : 'empty',
     child_process: 'empty'
   }
 }
